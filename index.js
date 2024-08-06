a920d1d9bcba2fb48940a68fa7c60a9d953192e8
Sanjay Balam <b.sanjay0701@gmail.com>
2024-08-06 16:58:20 +0530

    initial

--- index.js
+++ index.js
@@ -40,5 +40,5 @@ app.use("/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, console.log("Server is Running..."));
